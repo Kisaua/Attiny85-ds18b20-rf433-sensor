0e8c48238721a1cffb701219e47e2d50fc2a51af
kisaua <rudenkiy@gmail.com>
2015-02-23 12:54:55 +0200

    clean the code from unused comments

--- blinky.c
+++ blinky.c
@@ -100,18 +100,14 @@ int main(void)
 unsigned int temp = 0;
 	cli();
 	     DDRB |= (1<<4);
-		//DDRB |= SENDER_PIN; // PB4 for the sender or Control LED
 		DDRB &= ~(1 << PB4); //PB3 as input
 		static oneWireDevice devices[MAX_DEVICES];
 		oneWireDevice *ds18b20;
-		//init_Timer_OOK();
 		_delay_us(550);
 		oneWireInit(BUS);
 		while (oneWireSearchBuses(devices, MAX_DEVICES, BUS) != ONEWIRE_SEARCH_COMPLETE);
 		ds18b20 = oneWireFindFamily(DS18B20_FAMILY_ID, devices, MAX_DEVICES);
 	sei();
- // PORTB is output, all pins
-   //PORTB = 0x00; // Make pins low to start
 
 for ( ; ; ) {
   if (loop_count % 15 == 0) {
@@ -119,32 +115,19 @@ for ( ; ; ) {
       for (int j = 0; j < 5; j++ ) {
       uint8_t vccx10 = readVccVoltage();
      data [1] = vccx10;
-	  //data [1] = 10;
 	if (ds18b20 != NULL){
 			temp = ds18b20ReadTemperature(ds18b20->bus, ds18b20->id);
 			data [3] = (temp & 0xFF);
 			data [2] = ((temp>>8) & 0xFF); 
-			//data [2] = 255;
 			data [4] = oneWireDataCrc(data);
 			_delay_ms(1);
 			loop_count = 0;
 			transmitArray(&man, 5, data);
 			transmitArray(&man, 5, data);
 		}
-		
-    //PORTB = 0x00;
-    //_delay_ms(50);
-    //transmitArray(&man, 3, data);
-    //PORTB ^= (1<<PB4);
-    //PORTB = 0x00;
-    //_delay_ms(50);
-    //PORTB ^= (1<<4); // invert all the pins
-    //digitalWrite(3,HIGH) 
-    
       }
 	  }
 system_sleep();
-//_delay_ms(1000); // wait some time
 loop_count++;
 }
 return 0;
@@ -197,8 +180,7 @@ ISR(WDT_vect)
 {
  sleep_disable();          // Disable Sleep on Wakeup
  f_wdt = 1;  // set global flag
-  // Your code goes here...
-  // Whatever needs to happen every 1 second
+
   sleep_enable();           // Enable Sleep Mode
 	
 }
